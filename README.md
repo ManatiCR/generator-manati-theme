cb825690e06a6814d029b4822335dbdcef28cc45
Federico Figueroa <ffc.figueroa@gmail.com>
2015-09-22 15:25:22 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# generator-manati-theme [![Build Status](https://secure.travis-ci.org/ManatiCR/generator-manati-theme.png?branch=master)](https://travis-ci.org/ManatiCR/generator-manati-theme)
+
+> [Yeoman](http://yeoman.io) generator to create a base Drupal 7 Theme created by [Estudio Manatí](http://www.estudiomanati.com/)
+
+
+## Install
+To install generator-manati-theme from npm, run:
+
+```bash
+npm install -g generator-manati-theme
+```
+**This is not yet implemented as the generator has not been published yet and is not tested enough.**
+To give it a try right now clone this repo and run `npm link` inside, at your own risk.
+
+## Usage
+```bash
+yo manati-theme
+```
+
+You'll have to provide a theme name and a description, you can use plain text for the name and we'll do everything else.
+
+You'll get a nice Live Reload installed, served by grunt and understood by drupal to react to changes in your localhost dev enviroment.  You can opt out of this feature when you run the generator.
+
+## License
+
+MIT
---
+++ generators/app/index.js
@@ -0,0 +1,125 @@
+'use strict';
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+var yosay = require('yosay');
+var _ = require('lodash');
+
+module.exports = yeoman.generators.Base.extend({
+  prompting: function () {
+    var done = this.async();
+
+    // Have Yeoman greet the user.
+    this.log(yosay(
+      'Welcome to the super duper ' + chalk.red('Manatí Theme') + ' generator!'
+    ));
+
+    var prompts = [{
+      name: 'themeName',
+      message: 'What\'s the name of this theme?',
+      default: 'Manatí Theme'
+    },{
+      name: 'themeDescription',
+      message: 'Please enter a description:',
+      default: function(props) {
+        return props.themeName+' is the best!';
+      }
+    },{
+      type: 'confirm',
+      name: 'hasLiveReload',
+      message: 'Do you want livereload to be setup for you?',
+      default: true
+    }];
+
+    this.prompt(prompts, function (props) {
+      this.props = props;
+      /**
+       * Create snake case slug for Drupal
+       */
+      if (props.themeName) {
+        this.props.themeSlugName = _.snakeCase(props.themeName);
+      } else {
+        this.props.themeSlugName = 'manati_theme';
+      }
+
+      this.destinationRoot(this.destinationPath()+'/'+this.props.themeSlugName);
+
+      // To access props later use this.props.someOption;
+
+      done();
+    }.bind(this));
+  },
+
+  writing: {
+    base: function () {
+      this.fs.copyTpl(
+        this.templatePath('_package.json'),
+        this.destinationPath('package.json'),
+        this.props
+      );
+      this.fs.copyTpl(
+        this.templatePath('_theme.info'),
+        this.destinationPath(this.props.themeSlugName+'.info'),
+        this.props
+      );
+      this.fs.copyTpl(
+        this.templatePath('_template.php'),
+        this.destinationPath('template.php'),
+        this.props
+      );
+      this.fs.copyTpl(
+        this.templatePath('_theme-settings.php'),
+        this.destinationPath('theme-settings.php'),
+        this.props
+      );
+      this.fs.copy(
+        this.templatePath('templates'),
+        this.destinationPath('templates')
+      );
+    },
+
+    projectfiles: function () {
+      this.fs.copyTpl(
+        this.templatePath('_README.md'),
+        this.destinationPath('README.md'),
+        this.props
+      );
+      this.fs.copyTpl(
+        this.templatePath('_Gruntfile.js'),
+        this.destinationPath('Gruntfile.js'),
+        this.props
+      );
+      this.fs.copy(
+        this.templatePath('gitignore'),
+        this.destinationPath('.gitignore')
+      );
+      this.fs.copy(
+        this.templatePath('editorconfig'),
+        this.destinationPath('.editorconfig')
+      );
+      this.fs.copy(
+        this.templatePath('jshintrc'),
+        this.destinationPath('.jshintrc')
+      );
+      this.fs.copy(
+        this.templatePath('css'),
+        this.destinationPath('css')
+      );
+      this.fs.copy(
+        this.templatePath('sass'),
+        this.destinationPath('sass')
+      );
+      this.fs.copy(
+        this.templatePath('js'),
+        this.destinationPath('js')
+      );
+      this.fs.copy(
+        this.templatePath('src-js'),
+        this.destinationPath('src-js')
+      );
+    }
+  },
+
+  install: function () {
+    this.installDependencies();
+  }
+});
